cf6e57d1dc352ecdd2c210b464097486657b39c6
GiorgioM98 <marian-gio@libero.it>
2023-12-12 15:52:32 +0100

    codice javascript ridotto

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,93 +1,65 @@
-// creazione bottone crescente
-const btnPlus= document.createElement("button");
-btnPlus.setAttribute("class", "btnPlus");
-const textBtnPlus= document.createTextNode("+");
-btnPlus.appendChild(textBtnPlus);
-document.querySelector(".container").appendChild(btnPlus);
+    // aggiunta del DomContentLoad
+document.addEventListener("DOMContentLoaded", function(){
+    // funzione unica per creare elementi in html
+function creaNuovoElemento(tagName,classe, testo){
+    nuovoElemento= document.createElement(tagName);
+    nuovoElemento.classList.add(classe);
+    nuovoElemento.textContent= testo;
+    document.querySelector(".container").appendChild(nuovoElemento);
+    return nuovoElemento;
+}
+
+    // creazione elementi in html e creazione const btn da usare per gli eventi
+const btnPlus= creaNuovoElemento("button","btnPlus","+");
+const btnLess= creaNuovoElemento("button","btnLess","-");
+const btnReset=creaNuovoElemento("button","btnReset","Reset");
+const counterDisplay=creaNuovoElemento("div","counterDisplay","Conta le stelle!");
 
 
 
-// creazione bottone decrescente
-const btnLess= document.createElement("button");
-btnLess.setAttribute("class", "btnLess");
-const textBtnLess= document.createTextNode("-");
-btnLess.appendChild(textBtnLess);
-document.querySelector(".container").appendChild(btnLess);
+
+    //   funzione per aggiungere effetti al bottone
+function aggiungiEffettoBottone(btn){
+    // evento al passaggio del mouse
+    btn.addEventListener("mouseenter", function(){
+        btn.style.boxShadow= "0 0 20px 2px white";
+    });
+    // evento all'abbandono del bottone del mouse
+    btn.addEventListener("mouseleave", function(){
+        btn.style.boxShadow= "none";
+    })
+};
+
+    // aggiunta funzione evento ai bottoni
+aggiungiEffettoBottone(btnPlus);
+aggiungiEffettoBottone(btnLess);
+aggiungiEffettoBottone(btnReset);
 
 
-
-// creazione bottone reset
-const btnReset= document.createElement("button");
-btnReset.setAttribute("class", "btnReset");
-const textBtnReset= document.createTextNode("Reset");
-btnReset.appendChild(textBtnReset);
-document.querySelector(".container").appendChild(btnReset);
-
-
-// creazione display contatore
+    // evento listener addizione
 let somma= 0;
-const counterDisplay= document.createElement("div");
-counterDisplay.setAttribute("class", "counterDisplay")
-const textCounterDisplay= document.createTextNode("Conta le stelle!");
-counterDisplay.appendChild(textCounterDisplay);
-document.querySelector(".container").appendChild(counterDisplay);
-
-
-
-
-// effetto bottone mouseenter/leave
-btnPlus.addEventListener("mouseenter", function(){
-    btnPlus.style.boxShadow="0 0 20px 2px white";
-})
-btnPlus.addEventListener("mouseleave", function(){
-    btnPlus.style.boxShadow="none";
-})
-
-
-btnLess.addEventListener("mouseenter", function(){
-    btnLess.style.boxShadow="0 0 20px 2px white";
-})
-btnLess.addEventListener("mouseleave", function(){
-    btnLess.style.boxShadow="none";
-})
-
-
-btnReset.addEventListener("mouseenter", function(){
-    btnReset.style.boxShadow="0 0 20px 2px white";
-})
-btnReset.addEventListener("mouseleave", function(){
-    btnReset.style.boxShadow="none";
-})
-
-
-
-
-// evento listener addizione
 btnPlus.addEventListener("click", function(){
     somma= somma +1;
-    textCounterDisplay.nodeValue = somma;
+    counterDisplay.innerHTML= somma;
 })
 
-// evento listener sottrazione
+    // evento listener sottrazione con "if" nel caso somma fosse minore di zero
 btnLess.addEventListener("click", function(){
     somma= somma -1;
-    textCounterDisplay.nodeValue = somma;
+    if(somma<0){
+        somma=0;
+    }
+    counterDisplay.innerHTML= somma;
 })
 
-// evento listener contatore uguale a zero
-btnLess.addEventListener("click", function(){
-    if(somma<0){
-        somma ++;
-        textCounterDisplay.nodeValue = somma;
-    }
+    // evento reset
+btnReset.addEventListener("click", function(){
+    somma= 0;
+    counterDisplay.innerHTML= somma;
+})
 });
 
 
-// evento reset
-btnReset.addEventListener("click", function(){
-    somma= 0;
-    textCounterDisplay.nodeValue= somma;
-})
 
 
 
