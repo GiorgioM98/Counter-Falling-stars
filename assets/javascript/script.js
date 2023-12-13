5aa2cc6e4ec83a03c8e49986a6682dc876b4cba1
GiorgioM98 <marian-gio@libero.it>
2023-12-13 20:55:47 +0100

    modificato file javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,48 +17,52 @@ const counterDisplay=creaNuovoElemento("div","counterDisplay","Conta le stelle!"
 
 
 
+// "MOUSEENTER" NON FUNZIONA IN QUESTO CASO, TUTTI GLI ALTRI EVENT SI' INVECE, NON SONO RIUSCITO A CAPIRE IL MOITVO
 
-    //   funzione per aggiungere effetti al bottone
-function aggiungiEffettoBottone(btn){
-    // evento al passaggio del mouse
-    btn.addEventListener("mouseenter", function(){
-        btn.style.boxShadow= "0 0 20px 2px white";
+    // Creazione funzione sul container per ascoltare tutti gli eventi dentro
+    document.querySelector(".container").addEventListener("mouseenter", function(event) {
+        const target = event.target;
+    
+        // Verifica se l'elemento di destinazione ha la classe "btnPlus" o "btnLess" o "btnReset"
+        if (target.classList.contains("btnPlus") || target.classList.contains("btnLess")  || target.classList.contains("btnReset")) {
+            target.style.boxShadow = "0 0 20px 2px white";
+        } else {
+            // Rimuovi l'ombra dagli altri elementi
+            document.querySelectorAll(".btnPlus, .btnLess").forEach(function(btn) {
+                btn.style.boxShadow = "none";
+            });
+        };
     });
-    // evento all'abbandono del bottone del mouse
-    btn.addEventListener("mouseleave", function(){
-        btn.style.boxShadow= "none";
-    })
-};
-
-    // aggiunta funzione evento ai bottoni
-aggiungiEffettoBottone(btnPlus);
-aggiungiEffettoBottone(btnLess);
-aggiungiEffettoBottone(btnReset);
-
-
-    // evento listener addizione
-let somma= 0;
-btnPlus.addEventListener("click", function(){
-    somma= somma +1;
-    counterDisplay.innerHTML= somma;
-})
-
-    // evento listener sottrazione con "if" nel caso somma fosse minore di zero
-btnLess.addEventListener("click", function(){
-    somma= somma -1;
-    if(somma<0){
-        somma=0;
-    }
-    counterDisplay.innerHTML= somma;
-})
-
-    // evento reset
-btnReset.addEventListener("click", function(){
-    somma= 0;
-    counterDisplay.innerHTML= somma;
-})
-});
-
+        
+    
+    
+    // dichiarazione della variabile somma inizializzata a 0
+    let somma= 0;
+    // creazione funzione sul container per ascoltare tutti gli eventi dentro
+    document.querySelector(".container").addEventListener("click", function(event){
+        const target = event.target;
+    
+             // Verifica se l'evento proviene da un pulsante all'interno del container
+        if (target.closest(".container")) {
+            // evento bottone addizione
+            if(target.classList.contains("btnPlus")){
+                somma++;
+                // evento bottone sottrazione con "if" nel caso somma fosse minore di zero
+            }else if(target.classList.contains("btnLess")){
+                somma--;
+                if(somma<0){
+                    somma=0;
+                }
+                // evento bottone reset
+            }else if(event.target.classList.contains("btnReset")){
+                somma=0;
+            }
+            // aggiunta di somma a counterdisplay per visualizzare il numero corrente
+            counterDisplay.innerHTML= somma;
+         };
+    });
+    });
+   
 
 
 
